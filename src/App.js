137815a06f59a229018b90c798d31cd37562d98b
Katia <katiascsccia@gmail.com>
2026-05-31 15:52:48 +0200

    aggiunto useState e props al componente Headers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import "./CSS/utily.css";
 import { Header } from "./componenti/Header.js";
+import { useState } from "react";
 
 function App() {
+  // useState
+  const [ countActivity, setCountActivity ] = useState(0);
   return (
     <div className="App container">
-      <Header />
+      <Header count={countActivity} setCount={setCountActivity} />
     </div>
   );
 }
--- src/componenti/Header.js
+++ src/componenti/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "../CSS/rootEgeneral.css";
 
-export const Header = () => {
+export const Header = ({ count, setCount }) => {
   return (
     <>
       <h1>Le mie attività</h1>
-      <h3>Hai 3 task in sospeso per oggi</h3>
+      <h3>Hai {count} task in sospeso per oggi</h3>
     </>
   )
 }
