47934332392eee598fc53eb30d349d9efbe379fa
Katia <katiascsccia@gmail.com>
2026-05-31 16:22:22 +0200

    sviluppato e importato componente addNewTask

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
 import "./CSS/utily.css";
+import "./CSS/utily.css";
+import "./App.css";
+import { AddNewTask } from "./componenti/AddNewTask.js";
 import { Header } from "./componenti/Header.js";
 import { useState } from "react";
 
 function App() {
   // useState
-  const [ countActivity, setCountActivity ] = useState(0);
+  const [countActivity, setCountActivity] = useState(0);
   return (
     <div className="App container">
-      <Header count={countActivity} setCount={setCountActivity} />
+      <div className="contenitorePrincipale flex">
+        <div className="box1">
+          <Header count={countActivity} setCount={setCountActivity} />
+        </div>
+        <div className="box2">
+          <AddNewTask />
+        </div>
+      </div>
     </div>
   );
 }
--- src/componenti/AddNewTask.js
+++ src/componenti/AddNewTask.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import "../CSS/addNewTask.css";
 
 export const AddNewTask = () => {
   return (
-    <div>AddNewTask</div>
+    <>
+      <div className='newTask flex'>
+        <input type='text' placeholder='Aggiungi una nuova attività...'
+        className='input_newTask'/>
+        <button className='btn_newTask'>Aggiungi</button>
+      </div>
+    </>
   )
 }
