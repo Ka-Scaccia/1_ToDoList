98a193fd328b983104127f47b1de197301474398
Katia <katiascsccia@gmail.com>
2026-06-01 15:31:38 +0200

    aggiunto useState e props a Task

--- src/App.js
+++ src/App.js
@@ -9,11 +9,25 @@ import { useState } from "react";
 
 function App() {
   // useState
+  // contatore task
   const [countActivity, setCountActivity] = useState(0);
   const [activity, setActivity] = useState("");
+  // controllo click btn "Aggiungi"
   const [isClicked, setIsClicked] = useState(false);
   const yesClicked = () => setIsClicked(true);
   const noClicked = () => setIsClicked(false);
+  // controllo click task fatto
+  const [isChecked, setIsChecked] = useState(false);
+  const yesChecked = () => setIsChecked(true);
+  const noChecked = () => setIsChecked(false);
+  // controllo click su icona edit
+  const [isClickedEdit, setIsClickedEdit] = useState(false);
+  const yesEdit = () => setIsClickedEdit(true);
+  const noEdit = () => setIsClickedEdit(false);
+  // controllo click su icona delete
+  const [isClickedDelete, setIsClickedDelete] = useState(false);
+  const yesDelete = () => setIsClickedDelete(true);
+  const noDelete = () => setIsClickedDelete(false);
   return (
     <div className="App container">
       <div className="contenitorePrincipale flex">
@@ -21,11 +35,15 @@ function App() {
           <Header count={countActivity} setCount={setCountActivity} />
         </div>
         <div className="box2">
-          <AddNewTask content={activity} setContent={setActivity}
-          clickedBtn={yesClicked} />
+          <AddNewTask
+            content={activity}
+            setContent={setActivity}
+            clickedBtn={yesClicked}
+          />
         </div>
         <div className="box3">
-          <Task />
+          <Task OnChecked={yesChecked} OnEdit={yesEdit}
+          onDelete={yesDelete} />
         </div>
       </div>
     </div>
--- src/componenti/Task.js
+++ src/componenti/Task.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../CSS/task.css";
 
-export const Task = () => {
+export const Task = ({ OnChecked, OnEdit, onDelete }) => {
   return (
     <>
       <div className="task flex">
@@ -18,6 +18,7 @@ export const Task = () => {
             stroke-linejoin="round"
             class="lucide lucide-square-icon lucide-square
             unchecked"
+            onClick={OnChecked}
           >
             <rect width="18" height="18" x="3" y="3" rx="2" />
           </svg>
@@ -36,6 +37,7 @@ export const Task = () => {
             stroke-linejoin="round"
             class="lucide lucide-pencil-icon lucide-pencil
             edit"
+            onClick={OnEdit}
           >
             <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
             <path d="m15 5 4 4" />
@@ -52,6 +54,7 @@ export const Task = () => {
             stroke-linejoin="round"
             class="lucide lucide-trash-icon lucide-trash
             remove"
+            onClick={onDelete}
           >
             <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
             <path d="M3 6h18" />
